e6b9117f8466328184fff5d2ba1a93c6123a3593
Perry Obara <perryobara@gmail.com>
2019-06-17 15:09:58 +0300

    Updated tests to check if getter methods return valid types

--- src/test/java/NavigateTest.java
+++ src/test/java/NavigateTest.java
@@ -3,9 +3,19 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class NavigateTest {
-@Test
-    public void decodeString_checkInputTypes_typeOf(int selection){
-    Navigate testNavigate=new Navigate(selection);
-    assertEquals(2,testNavigate.getSelection());
-}
-}
+
+
+    @Test
+    public void getmWord_returntypeIfString_String() {
+        Navigate testGetMWord=new Navigate(1,3,"");
+       String testWord = "";
+        assertEquals(true,testGetMWord.getmWord() instanceof String);
+    }
+    @Test
+    public void getmKey_returnTypeIfInt_Integer(){
+        Navigate testGetMKey=new Navigate(1,8,"");
+        Integer key=8;
+        assertEquals(true, key instanceof Integer);
+    }
+    }
+
